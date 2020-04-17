831951aefa1c1544dcd5cf54c9ade29942b319ac
dorotarget <63640426+dorotarget@users.noreply.github.com>
2020-04-17 22:20:00 +0200

    feierabend: formular funzt

--- cgi-bin/bestellung_keineanmeldung.py
+++ cgi-bin/bestellung_keineanmeldung.py
@@ -5,66 +5,37 @@ from pymysql import *
 
 
 class Bestellung_aufgeben:
-      def __init__(self):
-         self.name=""
-         self.groesze=""
-         self.beschreibung=""
-         self.preis=""
+    def __init__(self):
+         self.bestellungsid = -1 # besser: "Magic Number", zu faul :-O
+    
+    def bestellung_auswerten(self):
+        form = cgi.FieldStorage()
 
-      def db(self):
-         db_connection = mysql.connector.connect(host="localhost", user="root", passwd="")
-         cursor_pizza = db_connection.cursor()
-         try:
-            cursor_pizza.execute("use pizzastars")
-         except (Exception):
-            cursor_pizza.execute("create database pizzastars; use pizzastars")
-         while 1:
-             form = cgi.FieldStorage()
-             if "anzahl" in form:
-                  self.anzahl = form[""].value
-                  try:
-                      cursor_pizza.execute("insert into bestellung (bestellung_id) Values ('LAST_INSERT_ID(id+1)')") 
-                      db_connection.commit() 
-                  break
-                  #except (Exception):
-                  #cursor_pizza.execute("create table bestellung_id (pizza_id int not null auto_increment primary key, name varchar, groesze int, beschreibung varchar, preis double not null)")
-                  #continue
-         db_connection.close()
-         self.ausgabe()
+        number_of_rows = int(form["number_of_rows"].value)     # passed from form
+        for r in range(1,number_of_rows+1):    # for each row...
+           pizza_id = form["pizzaId"+ str(r)].value  
+           preis = form["preis"+ str(r)].value
+           anzahl = form["anzahl"+ str(r)].value
 
-      def insert_wird_bestellt(self):
-          db_connection = mysql.connector.connect(host="localhost", user="root", passwd="")
-          cursor_pizza = db_connection.cursor()
-          try:
-            cursor_pizza.execute("use pizzastars")
-          while 1:
-               try:
-                  cursor_pizza.execute("insert into wird_bestellt (bestellung_id) Values ('LAST_INSERT_ID(id+1)')") 
-                  db_connection.commit() 
-                  break
-               except (Exception):
-                  cursor_pizza.execute("create table bestellung_id (pizza_id int not null auto_increment primary key, name varchar, groesze int, beschreibung varchar, preis double not null)")
-                  continue
-          db_connection.close()
-          self.ausgabe()
+        # TODO hier insert in die Datenbank
+        # check https://stackoverflow.com/questions/2548493/how-do-i-get-the-id-after-insert-into-mysql-database-with-python
+        # connection.insert_id()
+        # close db!
 
-               
-      def ausgabe(self):
-            print ("Content-Type: text/html")
-            print()
-            print( '<!DOCTYPE html>')
-            print ('<head>\
-                  <link rel="stylesheet" type="text/css" href="../cssclasse.css"/>\
-                  </head>\
-                  <body>\
-                  <form action="http://localhost:8888/cgi-bin/wird_bestellt_einfügen.py">\
-                  <input type="submit" value="Ohne Anmeldung fortfahren" onclick = "history.back()" />\
-                  </p>\
-                  </form>\
-                  </body>\
-                  </html>')
-            self.db()
+        self.ausgabe()
 
-objekt=Bestellung_aufgeben()
-objekt.auswertung()
+    def ausgabe(self):
+        # TODO SELECT bestellung und ausgeben (wie pizzaliste) mit self.bestellungsid
+        print ("Content-Type: text/html")
+        print()
+        print( '<!DOCTYPE html>')
+        print ('<head>\
+                <link rel="stylesheet" type="text/css" href="../cssclass.css"/>\
+                </head>\
+                <body>\
+                <h2>Bestellung erfolgreich!</h2>\
+                </body>\
+                </html>')
 
+bestellung = Bestellung_aufgeben()
+bestellung.bestellung_auswerten()
--- cgi-bin/pizzaliste.py
+++ cgi-bin/pizzaliste.py
@@ -10,23 +10,22 @@ class Pizzaliste:
       def pizzaliste_ausgeben(self):
         db_connection_pizzastars = mysql.connector.connect(host="localhost", user="root", passwd="")  
         query_db = ("USE pizzastars")
-        query_all_pizzas = ("SELECT name, groesze, beschreibung, preis FROM pizza")
+        query_all_pizzas = ("SELECT pizza_id, name, groesze, beschreibung, preis FROM pizza")
         print ("Content-Type: text/html")
         print()      
         print('<!DOCTYPE html>')
-        print('<head>\
-                <title>Bitte Pizza ausw&auml;hlen</title>\
-                    <\head>\
-                        <body>\
-                            <table>\
-                                <th>Pizza</th><th>Gr&ouml;sse</th><th>Beschreibung></th><th>Preis</th><th>Anzahl</th>')
+        print('<head><title>Bitte Pizza ausw&auml;hlen</title></head>\
+                <body>\
+                    <table>\
+                    <th>Nr.</th><th>Pizza</th><th>Gr&ouml;sse</th><th>Beschreibung</th><th>Preis</th><th>Anzahl</th>')
         db_cursor = db_connection_pizzastars.cursor()
         db_cursor.execute(query_db)
         db_cursor.execute(query_all_pizzas)
         pizza_row=""
-        for(name,groesze, beschreibung, preis) in db_cursor:
+        for(pizza_id, name, groesze, beschreibung, preis) in db_cursor:
+            converted_pizza_id = str(pizza_id)
             converted_preis = "{:.2f}".format(2, preis)
-            pizza_row='<tr> <td>'+name+'</td><td>'+groesze+'</td><td>'+beschreibung+'</td><td>'+converted_preis+'</td><td>0!</td></tr>'
+            pizza_row='<tr><td>'+converted_pizza_id+'</td><td>'+name+'</td><td>'+groesze+'</td><td>'+beschreibung+'</td><td>'+converted_preis+'</td><td>0!</td></tr>'
             print(pizza_row)
         print('</table>\
             </body>')
